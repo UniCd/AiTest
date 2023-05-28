1a3590c2ad5c0ae6aa41462e8cf2bc3f81199696
zzj123321 <394487803@qq.com>
2023-05-28 17:06:47 +0800

    test

--- imagebuffermanager_autogen/4RV77YMG6H/moc_imagebuffermanager.cpp.d
+++
@@ -1,3 +0,0 @@
-D:/CppCode/Qt/QtApplications/github/test/imagebuffermanager_autogen/4RV77YMG6H/moc_imagebuffermanager.cpp: D:/CppCode/Qt/QtApplications/github/AI_Core/imagebuffermanager.h \
-  D:/CppCode/Qt/QtApplications/github/test/imagebuffermanager_autogen/moc_predefs.h \
-  D:/CppCode/Qt/QtApplications/github/AI_Core/AI_Core_global.h
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-﻿#include "imagebuffermanager_test.h"
-#include "gtest/gtest.h"
+﻿#include "gtest/gtest.h"
+#include "imagebuffermanager_test.h"
+#include "TestEnvironment.h"
 
 
 int main(int argc, char **argv) {
+    testing::AddGlobalTestEnvironment(new TestEnvironment);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
